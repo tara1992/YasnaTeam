10343ecce0502fe3fb8df7eac62f78a934976c85
tara1992 <t.babaei1992@gmail.com>
2022-04-29 01:47:41 +0430

    change blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,26 +2,37 @@
   <div>
     <b-container fluid>
       <b-row>
-        <b-col cols="12">
+        <b-col cols="12" class="category-title">
           <h1> دسته بندی ها</h1>
         </b-col>
       </b-row>
-      <b-row>
-        <b-col lg="2" md="2" sm="2"  v-for="category in categories" :key="category.id">
-          <div class="mini-card">
-            <img :src="category[0].link" alt="image"/>
+      <div class="category-container">
+        <div class="d-flex justify-content-around">
+          <div class="mini-card" v-for="image in images" :key="image.id">
+            <img :src="image.pic" alt="image"/>
             <div class="text">
-              <p>{{ category.title }}</p>
+              <p>{{ image.caption }}</p>
             </div>
           </div>
+        </div>
+      </div>
 
-        </b-col>
-      </b-row>
+
+
+        <!--        <b-col lg="2" md="2" sm="2"  v-for="category in categories" :key="category.id">-->
+        <!--          <div class="mini-card">-->
+        <!--            <img :src="category[0].link" alt="image"/>-->
+        <!--            <div class="text">-->
+        <!--              <p>{{ category.title }}</p>-->
+        <!--            </div>-->
+        <!--          </div>-->
+        <!--        </b-col>-->
+
       <b-row>
-        <b-col lg="4" md="4" sm="1" v-for="card in cardBody" :key="card.id" class="custom-card">
-          <b-card>
+        <b-col lg="4" md="6" sm="1" v-for="card in cardBody" :key="card.id" class="custom-card">
+          <b-card >
             <div class="header">
-              <img src='@/assets/img/card-pic-1.svg' alt="card"/>
+              <img :src='card.pic' alt="card"/>
               <div class="tag">
                 <span>آشپزی</span>
               </div>
@@ -47,19 +58,18 @@
             </div>
           </b-card>
         </b-col>
-
       </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'Blog',
   components: {},
-  computed :{
+  computed: {
     ...mapGetters({
       categories: 'categories',
       posts: 'posts',
@@ -68,16 +78,16 @@ export default {
   data() {
     return {
       images: [
-        {id: 1, pic: require('@/assets/img/mini-card-1.svg'), caption: 'آشبزی'},
-        {id: 2, pic: require('@/assets/img/mini-card-2.svg'), caption: 'سبک زندگی'},
-        {id: 3, pic: require('@/assets/img/mini-card-3.svg'), caption: 'سلامت'},
-        {id: 4, pic: require('@/assets/img/mini-card-4.svg'), caption: 'دانستنی'},
-        {id: 5, pic: require('@/assets/img/mini-card-5.svg'), caption: 'اخبار ترخینه'},
-        {id: 6, pic: require('@/assets/img/mini-card-6.svg'), caption: 'تازه ها'},
+        {id: 1, pic: require('@/assets/img/mini-card-1.jpg'), caption: 'آشبزی'},
+        {id: 2, pic: require('@/assets/img/mini-card-2.jpg'), caption: 'سبک زندگی'},
+        {id: 3, pic: require('@/assets/img/mini-card-3.jpg'), caption: 'سلامت'},
+        {id: 4, pic: require('@/assets/img/mini-card-4.jpg'), caption: 'دانستنی'},
+        {id: 5, pic: require('@/assets/img/mini-card-5.jpg'), caption: 'اخبار ترخینه'},
+        {id: 6, pic: require('@/assets/img/mini-card-6.jpg'), caption: 'تازه ها'},
       ],
       cardBody: [
         {
-          id: 1, pic: require('@/assets/img/card-pic-2.svg'), title: 'خواص گوشت بوقلمون',
+          id: 1, pic: require('@/assets/img/card-2.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-1.svg'),
           name: 'مهتاب میکائیلی',
@@ -85,7 +95,7 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 2, pic: require('@/assets/img/card-pic-1.svg'), title: 'خواص گوشت بوقلمون',
+          id: 2, pic: require('@/assets/img/card-5.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-2.svg'),
           name: 'علی رضوانی',
@@ -93,7 +103,7 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-3.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-3.svg'),
           name: 'لادن مینایی',
@@ -101,7 +111,7 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-1.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-3.svg'),
           name: 'لادن مینایی',
@@ -109,7 +119,7 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-4.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-3.svg'),
           name: 'لادن مینایی',
@@ -117,7 +127,7 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-5.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-3.svg'),
           name: 'لادن مینایی',
@@ -125,7 +135,7 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-1.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-3.svg'),
           name: 'لادن مینایی',
@@ -133,15 +143,15 @@ export default {
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-4.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
-          avatar: require('@/assets/img/avatar-3.svg'),
+          avatar: require('@/assets/img/avatar-1.svg'),
           name: 'لادن مینایی',
           date: '1400/07/29',
           comment: '5 دیدگاه'
         },
         {
-          id: 3, pic: require('@/assets/img/card-pic-3.svg'), title: 'خواص گوشت بوقلمون',
+          id: 3, pic: require('@/assets/img/card-5.jpg'), title: 'خواص گوشت بوقلمون',
           text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با...',
           avatar: require('@/assets/img/avatar-3.svg'),
           name: 'لادن مینایی',
@@ -152,9 +162,9 @@ export default {
       ]
     }
   },
-  beforeCreate(){
-    this.$store.dispatch('getCategories');
-    this.$store.dispatch('getPosts');
-  }
+  // beforeCreate(){
+  //   this.$store.dispatch('getCategories');
+  //   this.$store.dispatch('getPosts');
+  // }
 }
 </script>
